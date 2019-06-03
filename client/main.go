d4e45d2ea46ed64e1dfb84575d2e78719e4d5c50
Arinto Murdopo <arinto@gmail.com>
2019-06-03 16:52:42 +0700

    Try to use waitHello

--- client/main.go
+++ client/main.go
@@ -6,21 +6,58 @@ import (
 	pb "github.com/arinto/hello-concurrent-grpc-go/internal/helloworld"
 	"google.golang.org/grpc"
 	"log"
+	"os"
+	"sync"
 	"time"
 )
 
-func hello(ctx context.Context, client pb.GreeterClient,  name *string, resChan chan *pb.HelloReply, errChan chan error) {
+func asyncHello(ctx context.Context, client pb.GreeterClient,  name *string, resChan chan *pb.HelloReply, errChan chan error) {
 	defer close(resChan)
 	defer close(errChan)
 
 	r, err := client.SayHello(ctx, &pb.HelloRequest{Name: *name})
 	if err != nil {
 		log.Println(err)
-		log.Printf("Sending to err channel: %+v\n", r)
 		errChan <- err
 	}
-	log.Printf("Sending to result channel: %+v\n", r)
 	resChan <- r
+}
+
+func waitHello(helloId string, wg *sync.WaitGroup,  ctx context.Context, resChan chan *pb.HelloReply, errChan chan error) {
+	select {
+	case r := <- resChan:
+		log.Printf("Greeting: %s to %s", r.Message, helloId)
+	case err := <- errChan:
+		log.Printf("Error calling helloworld %s: %+v", helloId, err)
+	case <-ctx.Done():
+		log.Printf("Timeout calling helloworld %s", helloId)
+	}
+	wg.Done()
+}
+
+func anExperiment(expId int, timeout1 *time.Duration, timeout2 *time.Duration, c1 pb.GreeterClient, c2 pb.GreeterClient, address1 *string, address2 *string, name *string){
+	ctx := context.Background()
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	// First async grpc call
+	ctx1, cancel1 := context.WithTimeout(ctx, *timeout1)
+	defer cancel1()
+	resChan1 := make(chan *pb.HelloReply, 1)
+	errChan1 := make(chan error, 1)
+	go asyncHello(ctx1, c1, name, resChan1, errChan1)
+	go waitHello(*address1, &wg, ctx1, resChan1, errChan1)
+
+	// Second async grpc call
+	ctx2, cancel2 := context.WithTimeout(ctx, *timeout2)
+	defer cancel2()
+	resChan2 := make(chan *pb.HelloReply, 1)
+	errChan2 := make(chan error, 1)
+	go asyncHello(ctx2, c2, name, resChan2, errChan2)
+	go waitHello(*address2, &wg, ctx2, resChan2, errChan2)
+	wg.Wait()
+	time.Sleep(100 * time.Millisecond)
+	log.Printf("Done for experiment: %d\n", expId)
 }
 
 func main() {
@@ -28,9 +65,12 @@ func main() {
 	address2 := flag.String("address2", "localhost:50052", "Address of the 2nd gRPC server")
 	timeout1 := flag.Duration("timeout1", 200*time.Millisecond, "Timeout for 1st gRPC call")
 	timeout2 := flag.Duration("timeout2", 200*time.Millisecond, "Timeout for 2nd gRPC call")
+	n := flag.Int("n", 10, "Number of experiment")
 
-	name := flag.String("name", "hello-concurrent", "Name to be greet")
+	name := flag.String("name", "concurrent", "Name to be greet")
 	flag.Parse()
+
+	log.SetOutput(os.Stdout)
 
 	conn1, err := grpc.Dial(*address1, grpc.WithInsecure())
 	if err != nil {
@@ -47,41 +87,8 @@ func main() {
 	defer conn2.Close()
 	c2 := pb.NewGreeterClient(conn2)
 
-	ctx := context.Background()
-
-	// First async grpc call
-	ctx1, cancel1 := context.WithTimeout(ctx, *timeout1)
-	defer cancel1()
-	resChan1 := make(chan *pb.HelloReply, 1)
-	errChan1 := make(chan error, 1)
-	go hello(ctx1, c1, name, resChan1, errChan1)
-
-	// Second async grpc call
-	ctx2, cancel2 := context.WithTimeout(ctx, *timeout2)
-	defer cancel2()
-	resChan2 := make(chan *pb.HelloReply, 1)
-	errChan2 := make(chan error, 1)
-	go hello(ctx2, c2, name, resChan2, errChan2)
-
-
-	select {
-	case <-ctx1.Done():
-		log.Println("Timeout calling helloworld1")
-	case error1 := <- errChan1:
-		log.Printf("Error calling helloworld1: %+v", error1)
-	case r1 := <- resChan1:
-		log.Printf("Greeting: %s", r1.Message)
+	for i := 0; i < *n; i++ {
+		anExperiment(i, timeout1, timeout2, c1, c2, address1, address2, name)
 	}
-
-	select {
-	case <-ctx2.Done():
-		log.Println("Timeout calling helloworld2")
-	case error2 := <- errChan2:
-		log.Printf("Error calling helloworld2: %+v", error2)
-	case r2 := <- resChan2:
-		log.Printf("Greeting: %s", r2.Message)
-	}
-
-	time.Sleep(1*time.Second)
 
 }
--- server/main.go
+++ server/main.go
@@ -8,27 +8,35 @@ import (
 	"github.com/opentracing/opentracing-go"
 	"google.golang.org/grpc"
 	"log"
+	"math/rand"
 	"net"
 	"time"
 )
 
-type server struct{
-	id string
-	sleep time.Duration
+type server struct {
+	id        string
+	sleepms   int
+	randomize bool
 }
 
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received %+v, sleep for %+v\n", in.Name, s.sleep)
-	time.Sleep(s.sleep)
+	sleepms := s.sleepms
+	if s.randomize {
+		sleepms = rand.Intn(s.sleepms)
+	}
+	log.Printf("Received %+v, sleepms for %+v ms\n", in.Name, sleepms)
+	time.Sleep(time.Duration(sleepms) * time.Millisecond)
 	log.Println("Done!")
 	return &pb.HelloReply{Message: s.id + ": Hello " + in.Name}, nil
 }
 
 func main() {
 	address := flag.String("address", ":50051", "Address of the gRPC server")
-	sleep := flag.Duration("sleepms", 100*time.Millisecond, "Duration of sleep in millis")
+	sleepms := flag.Int("sleepms", 100, "Duration of sleepms in millis")
+	randomize := flag.Bool("randomize", false, "Set to true to randomize sleepms")
 
 	flag.Parse()
+	rand.Seed(time.Now().Unix())
 
 	lis, err := net.Listen("tcp", *address)
 	if err != nil {
@@ -36,7 +44,7 @@ func main() {
 	}
 	s := grpc.NewServer(grpc.UnaryInterceptor(
 		otgrpc.OpenTracingServerInterceptor(opentracing.GlobalTracer(), otgrpc.LogPayloads())))
-	pb.RegisterGreeterServer(s, &server{id: *address, sleep: *sleep})
+	pb.RegisterGreeterServer(s, &server{id: *address, sleepms: *sleepms, randomize: *randomize})
 	log.Println("Start to serve, listening at " + *address)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
